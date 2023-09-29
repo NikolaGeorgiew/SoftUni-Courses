3026a66a24215b1ec51ebf96a1e0244d9506bb15
Nikola Georgiev <nikolageorgiev2000n@gmail.com>
2023-09-29 17:16:24 +0300

    Add files via upload

---
+++ JavaScript Front-End/01. JS Syntax Fundamentals/Exercise/More Exercise/02. Words Uppercase.js
@@ -0,0 +1,13 @@
+function solve(text) {
+    let searchedWord = /\w+/g;
+    let wordsArr = text.match(searchedWord);
+    let newArr = [];
+    
+
+    for (let i = 0; i < wordsArr.length; i++) {
+        newArr.push(wordsArr[i].toUpperCase());
+    }
+        
+
+    console.log(newArr.join(', '));
+}
---
+++ JavaScript Front-End/01. JS Syntax Fundamentals/Lab/16. Count String Occurrences.js
@@ -0,0 +1,12 @@
+function solve(text, search) {
+    let words = text.split(" ");
+    let counter = 0;
+
+    for (const word of words) {
+        if (word === search) {
+            counter++;
+        }
+    }
+    console.log(counter);
+}
+
